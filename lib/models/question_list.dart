7419a7b899022738f73392ada74593ff6f78920c
Bigbooty900 <84379477+Bigbooty900@users.noreply.github.com>
2021-05-18 14:44:51 +0300

    Update question_list.dart
    added questions and answers

--- lib/models/question_list.dart
+++ lib/models/question_list.dart
@@ -3,51 +3,51 @@ import 'question.dart';
 List<Question> myquestions = [
   // Question 1
   Question(
-    title: 'title',
-    description: 'description',
-    answer: 'answer',
-    options: ['options1', 'option2'],
+    title: 'Why does Cassius not like Caesar?',
+    description: '',
+    answer: 'Cassius sees Caesar as an unfit leader because he thinks he is weak.',
+    options: ['Cassius sees Caesar as an unfit leader because he thinks he is weak.', He does not like him because he is corrupt and unjust'],
     value: 1000,
   ),
   // Question 2
   Question(
-    title: 'title',
-    description: 'description',
-    answer: 'options1',
-    options: ['options1', 'option2'],
+    title: 'How did Cassius trick Brutus into conspiring against Caesar?',
+    description: '',
+    answer: 'He tricked Brutus by showing him fake letters that the citizens wrote but Brutus wrote the letters.',
+    options: ['Cassius gave Brutus a speech on how Caesar was weak', 'He tricked Brutus by showing him fake letters that the citizens wrote but Brutus wrote the letters.'],
     value: 1000,
   ), 
   // Question 3
   Question(
-    title: 'title',
-    description: 'description',
-    answer: 'options1',
-    options: ['options1', 'option2'],
+    title: 'Why would Caesar fear Cassius if he feared men?',
+    description: '',
+    answer: 'Cassius was a man that did not like being under ranked and that makes him dangerous. ',
+    options: ['Cassius was a man that did not like being under ranked and that makes him dangerous. ', 'Cassius never smiled.'],
     value: 1000,
   ), 
   // Question 4
   Question(
-    title: 'title',
-    description: 'description',
-    answer: 'options1',
-    options: ['options1', 'option2'],
+    title: 'Who is Calpurnia?',
+    description: '',
+    answer: 'Caesar’s wife.',
+    options: ['Caesar’s wife.', 'Cassius's wife'],
     value: 1000,
   ), 
   // Question 5
   Question(
-    title: 'title',
-    description: 'description',
-    answer: 'options1',
-    options: ['options1', 'option2'],
+    title: 'Who is Portia?',
+    description: '',
+    answer: 'Brutus’s wife.',
+    options: ['Brutus’s wife.', 'Cassius's wife '],
     value: 1000,
   ), 
   // Question 6
   Question(
-    title: 'title',
-    description: 'description',
-    answer: 'options1',
-    options: ['options1', 'option2'],
-    value: 1000,
+    title: 'What does Portia do to prove herself ?',
+    description: '',
+    answer: 'She harms herself to prove her strength.',
+    options: ['She lists off the things she has done to prove her strength', 'She harms herself to prove her strength.'],
+    value: 1500,
   ), 
   // Question 7
   Question(
